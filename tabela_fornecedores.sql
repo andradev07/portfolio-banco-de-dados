424b13dade46541b3eab495f65e2c73e4e42777d
andradev07 <168363373+andradev07@users.noreply.github.com>
2025-10-23 21:07:34 -0300

    Update tabela_fornecedores.sql

--- tabela_fornecedores.sql
+++ tabela_fornecedores.sql
@@ -1,5 +1,8 @@
 CREATE TABLE fornecedores (
-  id INT AUTO_INCREMENT
-  nome VARCHAR(100)
-  telefone(50)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100),
+  telefone VARCHAR(50)
   );
+
+INSERT INTO fornecedores,
+VALUES ('Techbrasil', 22541178);
