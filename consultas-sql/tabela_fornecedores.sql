1128e5c0e4355a8d44081befcfd5e55faf022366
andradev07 <168363373+andradev07@users.noreply.github.com>
2025-10-23 21:29:28 -0300

    Update and rename 09_procedure.sql to tabela_fornecedores.sql
    Tabela de fornecedores de produto informatica

---
+++ consultas-sql/tabela_fornecedores.sql
@@ -0,0 +1,19 @@
+CREATE TABLE fornecedores (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100),
+  telefone VARCHAR(50)
+  );
+
+
+INSERT INTO fornecedores(nome, telefone)
+  VALUES ('EletronTech', '21581234'),
+('Moveletron', '21581345'),
+('GigaEletrônicos', '21581456'),
+('Casa do Eletrônico', '21581567'),
+('TechMóveis', '21581678'),
+('ElectroStore', '21581789'),
+('Móveis & Bits', '21581890'),
+('SmartHome Supplies', '21581901'),
+('Eletrônica Brasil', '21582012'),
+('Mundo Eletrônico', '21582123');
+  
