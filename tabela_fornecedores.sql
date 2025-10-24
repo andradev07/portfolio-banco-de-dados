f26a708378b8ac83e391fcfddbd1bc35523ec84d
andradev07 <168363373+andradev07@users.noreply.github.com>
2025-10-23 21:24:49 -0300

    Update tabela_fornecedores.sql
    Tabela de dados dos fornecedores

--- tabela_fornecedores.sql
+++ tabela_fornecedores.sql
@@ -5,4 +5,13 @@ CREATE TABLE fornecedores (
   );
 
 INSERT INTO fornecedores,
-VALUES ('Techbrasil', 22541178);
+VALUES ('EletronTech', '21581234'),
+('Moveletron', '21581345'),
+('GigaEletrônicos', '21581456'),
+('Casa do Eletrônico', '21581567'),
+('TechMóveis', '21581678'),
+('ElectroStore', '21581789'),
+('Móveis & Bits', '21581890'),
+('SmartHome Supplies', '21581901'),
+('Eletrônica Brasil', '21582012'),
+('Mundo Eletrônico', '21582123');
